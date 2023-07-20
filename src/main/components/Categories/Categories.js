4c630e3c1fd547225d1bb5c37b419454e2d82bf4
harshala <rinkusg@gmail.com>
2023-07-20 19:50:54 +0530

    delete category API done

--- src/main/components/Categories/Categories.js
+++ src/main/components/Categories/Categories.js
@@ -1,6 +1,6 @@
-import React, { Fragment, useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import axios from 'axios';
+import React, { Fragment, useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
 import {
   Row,
   Col,
@@ -10,139 +10,149 @@ import {
   Dropdown,
   ProgressBar,
   Button,
-} from "react-bootstrap";
+  Modal,
+} from 'react-bootstrap'
 import { toast } from 'react-toastify'
 import { RotatingLines } from 'react-loader-spinner'
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 const Categories = () => {
-  const [data, setData] = useState([]);
-  const [getAllCategoriesData,setGetAllCategoriesData] = useState([]);
+  const [getAllCategoriesData, setGetAllCategoriesData] = useState([])
   const [token, setToken] = useState() //auth token
+  const [uid, setUId] = useState() //user id save for delete
+  const [showModal, setShowModal] = useState(false) //delete button modal
 
   useEffect(() => {
-    let accessToken = window.localStorage.getItem("jwt_access_token");
-    setToken(accessToken);
-    getAllCourses();
-    console.log(getAllCategoriesData);
-  }, []);
+    let accessToken = window.localStorage.getItem('jwt_access_token')
+    setToken(accessToken)
+    getAllCategories()
+    console.log(getAllCategoriesData)
+  }, [])
 
-  const getAllCourses = async () => {
-    const jwtToken = window.localStorage.getItem("jwt_access_token");
-    const url = "http://127.0.0.1:8000/lms-service/categories";
+  const getAllCategories = async () => {
+    const jwtToken = window.localStorage.getItem('jwt_access_token')
+    const url = 'http://127.0.0.1:8000/lms-service/categories'
     try {
       const response = await axios.get(url, {
         headers: {
-          "Auth-Token": jwtToken,
+          'Auth-Token': jwtToken,
         },
-      });
+      })
 
       // Handle the response data here
-      console.log("getAllCourses", response.data);
-      setGetAllCategoriesData(response.data.data);
+      console.log('getAllCategories', response.data)
+      setGetAllCategoriesData(response.data.data)
     } catch (error) {
       // Handle errors if any
-      console.error("Error fetching data:", error);
+      console.error('Error fetching data:', error)
       toast.error('Failed to fetch Courses !') // Handle the error
-    }
-  };
-
-
-  const fetchData = () => {
-    fetch(`https://localhost:8000/categories`)
-    //This function is used while fetching data from FASTAPI (HTTP/HTTPS) 3.110.124.80
-      // .then((response) => response.json())
-      // .then((actualData) => {
-      //   console.log(actualData);
-      //   setUsers(actualData.data);
-      //   console.log(data);
-
-      // This is used for fetching data from json-server port 4000 fastAPI 8000
-      .then((res) => res.json())
-      .then((data) => {
-         console.log(data);
-         setData(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
-
-  // useEffect( async () => {
-  //   fetchData();
-  // }, []);
-
-  async function deleteOperation(id)
-  {
-    if (window.confirm('Are you sure?'))
-    {
-      let result=await fetch("https://localhost:8000/categories/"+id,{
-        method:'DELETE'
-      });
-      result=await result.json();
-      console.warn(result)
-      fetchData();
     }
   }
 
-  const chackbox = document.querySelectorAll(".bs_exam_topper input");
-  const motherChackBox = document.querySelector(".bs_exam_topper_all input");
+  const deleteOperation = (userId) => {
+    setShowModal(true)
+    console.log('inside delete user', userId)
+    setUId(userId)
+  }
+
+  const handleCatDelete = () => {
+    console.log('modal delete', uid)
+    const config = {
+      headers: {
+        'Auth-Token': token, // Attach the JWT token in the Authorization header
+      },
+    }
+    const requestBody = {
+      id: uid,
+    }
+    console.log('config', config, requestBody)
+    // Make the Axios DELETE request
+    axios
+      .delete(`http://127.0.0.1:8000/lms-service/delete_category`, {
+        ...config,
+        data: requestBody,
+      })
+      .then((response) => {
+        setShowModal(false)
+        console.log(response.data.status)
+        getAllCategories()
+        toast.success('Category deleted successfully!', {
+          position: toast.POSITION.TOP_RIGHT,
+        })
+      })
+      .catch((error) => {
+        // Handle the error
+        console.error(error)
+        toast.error('Failed to delete Category!', {
+          position: toast.POSITION.TOP_RIGHT,
+        })
+      })
+  }
+
+  const chackbox = document.querySelectorAll('.bs_exam_topper input')
+  const motherChackBox = document.querySelector('.bs_exam_topper_all input')
   const chackboxFun = (type) => {
     for (let i = 0; i < chackbox.length; i++) {
-      const element = chackbox[i];
-      if (type === "all") {
+      const element = chackbox[i]
+      if (type === 'all') {
         if (motherChackBox.checked) {
-          element.checked = true;
+          element.checked = true
         } else {
-          element.checked = false;
+          element.checked = false
         }
       } else {
         if (!element.checked) {
-          motherChackBox.checked = false;
-          break;
+          motherChackBox.checked = false
+          break
         } else {
-          motherChackBox.checked = true;
+          motherChackBox.checked = true
         }
       }
     }
-  };
+  }
   const svg1 = (
-    <svg width="20px" height="20px" viewBox="0 0 24 24" version="1.1">
-      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-        <rect x="0" y="0" width="24" height="24"></rect>
-        <circle fill="#000000" cx="5" cy="12" r="2"></circle>
-        <circle fill="#000000" cx="12" cy="12" r="2"></circle>
-        <circle fill="#000000" cx="19" cy="12" r="2"></circle>
+    <svg width='20px' height='20px' viewBox='0 0 24 24' version='1.1'>
+      <g stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
+        <rect x='0' y='0' width='24' height='24'></rect>
+        <circle fill='#000000' cx='5' cy='12' r='2'></circle>
+        <circle fill='#000000' cx='12' cy='12' r='2'></circle>
+        <circle fill='#000000' cx='19' cy='12' r='2'></circle>
       </g>
     </svg>
-  );
+  )
 
-  let history = useHistory();
+  let history = useHistory()
   return (
     <Fragment>
       <Row>
         <Col lg={12}>
           <Card>
             <Card.Header>
-              <Card.Title>Categories</Card.Title>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-							<Link to="/add-category">
-							<Button variant="primary">Add Categories</Button>
-							</Link>
-							<Link to="/courses" >
-                <h6>View Course Catalog&nbsp;&nbsp;<i class="bi bi-arrow-right-square-fill"></i>&nbsp;&nbsp;&nbsp;&nbsp;
+              <Card.Title>Categories</Card.Title>
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <Link to='/add-category'>
+                <Button variant='primary'>Add Categories</Button>
+              </Link>
+              <Link to='/courses'>
+                <h6>
+                  View Course Catalog&nbsp;&nbsp;
+                  <i class='bi bi-arrow-right-square-fill'></i>
+                  &nbsp;&nbsp;&nbsp;&nbsp;
                 </h6>
-							</Link>
+              </Link>
             </Card.Header>
             <Card.Body>
               <Table responsive>
                 <thead>
                   <tr>
-                    <th></th>
-                    <th className="width80">
+                    {/* <th></th> */}
+                    {/* <th className='width80'>
                       <strong>#</strong>
-                    </th>
+                    </th> */}
                     <th>
-                      <center><strong>NAME</strong></center>
+                      <center>
+                        <strong>NAME</strong>
+                      </center>
                     </th>
                     <th></th>
                     <th></th>
@@ -153,37 +163,38 @@ const Categories = () => {
                   </tr>
                 </thead>
                 <tbody>
-                {getAllCategoriesData?.map((item, index) => {
-               return (
-                  <tr key={index}>
-                    <td></td>
-                    <td>
-                      <strong>{item.id}</strong>
-                    </td>
-                    <td><center>{item.name}</center></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>
-                      <div className="d-flex">
-                        <Link
-                          to="/edit-category"
-                          className="btn btn-primary shadow btn-xs sharp me-1"
-                        >
-                          <i className="fas fa-pencil-alt"></i>
-                        </Link>
-                        <Link
-                          href="#"
-                          className="btn btn-danger shadow btn-xs sharp"
-                          onClick={()=>deleteOperation(item.id)}
-                        >
-                          <i className="fa fa-trash"></i>
-                        </Link>
-                      </div>
-                    </td>
-                  </tr>
-               );
-                })}
+                  {getAllCategoriesData?.map((item, index) => {
+                    return (
+                      <tr key={index}>
+                        {/* <td></td> */}
+                        {/* <td>
+                          <strong>{item.id}</strong>
+                        </td> */}
+                        <td>
+                          <center>{item.name}</center>
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                          <div className='d-flex'>
+                            <Link
+                              to='/edit-category'
+                              className='btn btn-primary shadow btn-xs sharp me-1'
+                            >
+                              <i className='fas fa-pencil-alt'></i>
+                            </Link>
+                            <div
+                              className='btn btn-danger shadow btn-xs sharp'
+                              onClick={() => deleteOperation(item.id)}
+                            >
+                              <i className='fa fa-trash'></i>
+                            </div>
+                          </div>
+                        </td>
+                      </tr>
+                    )
+                  })}
                 </tbody>
               </Table>
             </Card.Body>
@@ -191,10 +202,39 @@ const Categories = () => {
         </Col>
       </Row>
       <div>
-      <Button onClick={() => history.goBack()}>Back</Button>
+        <Button onClick={() => history.goBack()}>Back</Button>
       </div>
+      <Modal show={showModal} onHide={() => setShowModal(false)} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <strong>Are you sure you want to delete this Category?</strong>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='danger light' onClick={() => setShowModal(false)}>
+            Close
+          </Button>
+          <Button variant='btn btn-primary' onClick={handleCatDelete}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Fragment>
-  );
-};
+  )
+}
 
-export default Categories;
+export default Categories
+
+// async function deleteOperation(id)
+//   {
+//     if (window.confirm('Are you sure?'))
+//     {
+//       let result=await fetch("https://localhost:8000/categories/"+id,{
+//         method:'DELETE'
+//       });
+//       result=await result.json();
+//       console.warn(result)
+//       fetchData();
+//     }
+//   }
