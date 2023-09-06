f8babf9081f05bac22d7b1372ea765fb5b8b5e4d
Harshala <imperial.harshala@gmail.com>
2023-09-06 19:19:34 +0530

    working on user part - group tab

--- src/main/components/Users/UserGroups.js
+++ src/main/components/Users/UserGroups.js
@@ -1,23 +1,19 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
-import pic1 from "@images/Users/Groupsvg.svg";
-
-import {
-  Dropdown,
-  DropdownButton,
-  ButtonGroup,
-  Nav,
-  Button,
-  FormCheck,
-  Tab,
-  Tabs,
-} from "react-bootstrap";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { RotatingLines } from "react-loader-spinner";
+import { Button, Table, Tab, Tabs } from "react-bootstrap";
 
 const UserGroups = (props) => {
   const userId = props.match.params.id;
-  console.log({ userId });
   const [activeTab, setActiveTab] = useState("user-groups/:id");
-  let history = useHistory();
+  const [allGrps, setAllGrps] = useState([]);
+  const [totalGrpData, setTotalGrpData] = useState(0);
+  const [token, setToken] = useState(); //auth token
+  const [currentPage, setCurrentPage] = useState(1); // Current page number
+  const itemsPerPage = 10; // Number of items to display per page
+  const history = useHistory();
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
@@ -26,56 +22,49 @@ const UserGroups = (props) => {
 
   useEffect(() => {
     const currentPath = history.location.pathname;
-    const tab = currentPath.substring(1); // Remove the leading slash
+    const tab = currentPath.substring(1);
     setActiveTab(tab);
   }, [history.location.pathname]);
 
+  useEffect(() => {
+    let token = window.localStorage.getItem("jwt_access_token");
+    setToken(token);
+    getAllGroups();
+  }, []);
+
+  // User List Api
+  const getAllGroups = () => {
+    const jwtToken = window.localStorage.getItem("jwt_access_token");
+    const config = {
+      headers: {
+        "Auth-Token": jwtToken,
+      },
+      params: {
+        user_id: userId,
+      },
+    };
+    axios
+      .get("http://127.0.0.1:8000/user-tab2/fetch_groups_of_user", config)
+      .then((response) => {
+        const grps = response.data.data.user_ids;
+        setAllGrps(grps);
+        setTotalGrpData(response.data.data.user_ids.length);
+      })
+      .catch((error) => {
+        toast.error("Failed to fetch groups!!!");
+      });
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentData = allGrps.slice(startIndex, endIndex);
+
+  const handleAddGrp = () => {};
+
+  const handleRemoveGrp = () => {};
+
   return (
     <Fragment>
-      {/* <Nav>
-        <Nav.Item as='div' className='nav nav-tabs' id='nav-tab' role='tablist'>
-          <Link
-            as='button'
-            className='nav-link  nt-unseen'
-            id='nav-following-tab'
-            eventKey='Follow'
-            type='button'
-            to='edit-user/:id'
-          >
-            Info
-          </Link>
-          <Link
-            as='button'
-            className='nav-link  nt-unseen'
-            id='nav-following-tab'
-            eventKey='Follow'
-            type='button'
-            to='/user-courses-info'
-          >
-            Courses
-          </Link>
-          <Link
-            as='button'
-            className='nav-link  nt-unseen'
-            id='nav-following-tab'
-            eventKey='Follow'
-            type='button'
-            to='/user-groups'
-          >
-            Groups
-          </Link>
-          <Link
-            as='button'
-            className='nav-link  nt-unseen'
-            id='nav-following-tab'
-            eventKey='Follow'
-            type='button'
-            to='/user-files'
-          >
-            Files
-          </Link>
-        </Nav.Item>
-      </Nav> */}
       <div className="row">
         <div className="col-lg-12">
           <div className="card">
@@ -88,17 +77,107 @@ const UserGroups = (props) => {
               <Tab eventKey={`user-files/${userId}`} title="Files"></Tab>
             </Tabs>
             <div className="card-header">
-              <h4 className="card-title">All Groups </h4>
+              <h4 className="card-title">Groups</h4>
             </div>
+            <div className="card-body">
+              {currentData.length === 0 ? (
+                <div className="loader-container">
+                  <RotatingLines
+                    strokeColor="grey"
+                    strokeWidth="5"
+                    animationDuration="0.75"
+                    width="140"
+                    visible={true}
+                  />
+                </div>
+              ) : currentData.length > 0 ? (
+                <>
+                  <Table responsive>
+                    <thead>
+                      <tr>
+                        <th>
+                          <strong>GROUP</strong>
+                        </th>
 
-            <div className="row mb-5">
-              <div className="col-lg-3"></div>
-              <div className="col-lg-7">
-                <img src={pic1} height={500} width={500} />
-                <h4 className="mb-10">
-                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You
-                  have not added in any Group
-                </h4>
+                        <th>
+                          <center>
+                            <strong>OPTIONS</strong>
+                          </center>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {allGrps.map((data, index) => {
+                        return (
+                          <tr key={index}>
+                            <td>{data.groupname}</td>
+                            <td>
+                              <center>
+                                {data.user_group_enrollment_id === null ? (
+                                  <div
+                                    className="btn btn-primary shadow btn-xs sharp me-1"
+                                    title="Add to group"
+                                    onClick={(e) =>
+                                      handleAddGrp(e, data.group_id)
+                                    }>
+                                    <i className="fa-solid fa-plus"></i>
+                                  </div>
+                                ) : (
+                                  <div
+                                    className="btn btn-danger shadow btn-xs sharp"
+                                    title="Remove from group"
+                                    onClick={(e) =>
+                                      handleRemoveGrp(
+                                        e,
+                                        data.user_group_enrollment_id
+                                      )
+                                    }>
+                                    <i className="fa-solid fa-minus"></i>
+                                  </div>
+                                )}
+                              </center>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </Table>
+                </>
+              ) : (
+                <>
+                  <div>
+                    <p className="text-center fs-20 fw-bold">
+                      No Course Found.
+                    </p>
+                  </div>
+                </>
+              )}
+              <br />
+              <div className="pagination-down">
+                <div className="d-flex align-items-center  ">
+                  <h4 className=" ">
+                    Showing <span>1-10 </span>from <span>{totalGrpData} </span>
+                    data
+                  </h4>
+                  <div className="d-flex align-items-center ms-auto mt-2">
+                    <Button
+                      className="mr-2"
+                      onClick={() => setCurrentPage(currentPage - 1)}
+                      disabled={currentPage === 1}>
+                      Previous
+                    </Button>
+                    &nbsp;&nbsp;
+                    <span className=" fs-18 fw-bold ">
+                      Page {currentPage} &nbsp;&nbsp;
+                    </span>
+                    <Button
+                      className="ml-2"
+                      onClick={() => setCurrentPage(currentPage + 1)}
+                      disabled={endIndex >= allGrps.length}>
+                      Next
+                    </Button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
